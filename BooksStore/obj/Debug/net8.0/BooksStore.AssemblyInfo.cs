880f3a7074836eb543f560467ca9d2137847f5b7
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-04-29 17:35:49 -0400

    updates

--- BooksStore/obj/Debug/net8.0/BooksStore.AssemblyInfo.cs
+++ BooksStore/obj/Debug/net8.0/BooksStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5581f4ce5815fcd619bb3a62152ff0ab60bb3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80ccc2d4d8c0b27c362d0481a621df9b764637a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
