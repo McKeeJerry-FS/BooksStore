a50f3633f45848d22d0664728dc7065f3d9331c6
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-04-29 20:17:09 -0400

    created a modal

--- BooksStore/obj/Debug/net8.0/BooksStore.AssemblyInfo.cs
+++ BooksStore/obj/Debug/net8.0/BooksStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880f3a7074836eb543f560467ca9d2137847f5b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682cf55206b94fdbbafd2b1cc5a89195602afd18")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
